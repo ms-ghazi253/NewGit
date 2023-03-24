d051e6eced9a0b8fadace2ab21aaf7e933946e70
unknown <ms.ghazi253@gmail.com>
2023-03-24 02:45:15 -0700

    switch case

--- push.js
+++ push.js
@@ -1,14 +1,19 @@
 
 'use strict'
 
-var a =5;
-var b =4;
+var num =3;
+switch(num){
+    case 10:
+        console.log('print 10');
+        break;
+    case 9 :
+        console.log("print 9");
+        break;
+    case 8:{
+        console.log('this with block');
+        break;}
 
-//&&
-// ||
-// !
-if(a==5 && b==6){
-    console.log('hello roocket');
-}else{
-    console.log("it's wrong")
+    default:
+        console.log('its wrong');
+        break;
 }
