1be5a2cf1e931edd52b551e5b86e161d56c3825e
unknown <ms.ghazi253@gmail.com>
2023-03-24 04:41:11 -0700

    forLoop

--- push.js
+++ push.js
@@ -1,35 +1,12 @@
 
 'use strict'
 
-var num =3;
-switch(num){
-    case 10:
-        console.log('print 10');
-        break;
-    case 9 :
-        console.log("print 9");
-        break;
-    case 8:{
-        console.log('this with block');
-        }
-        break;
+// var arr = [1,2,3,4,5,6]
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(arr[i])
+// }
 
-    default:
-        console.log('its wrong');
-        break;
-}
-
-var val = 11;
-switch(true){
-    case(val == 10 || val == 9):{
-        console.log('print yes');
-        break;
-    }
-    case(val >= 11):{
-        console.log('print 11+')
-        break;
-    }
-    default:
-        console.log('it`s wrong');
-        break;
+for(var i=0 ;i<5 ;i++ ){
+    alert(i)
 }
